3ea1bfae5cf0dd70bd038d576b9a4e3f7596ac6b
zyd2001 <zyd200101@outlook.com>
2021-03-26 22:17:29 +0800

    RT-27 For easier debugging

--- Software/Simulator/SimulatorTest/UnitTest1.cs
+++ Software/Simulator/SimulatorTest/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulatorCore;
+using System.IO;
+using System;
 
 namespace SimulatorTest
 {
@@ -8,7 +11,8 @@ namespace SimulatorTest
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.Fail();
+            RegisterFile<Scalar> sRF = new RegisterFile<Scalar>(16);
+            Console.WriteLine(sRF[0].f); // use R0 as float
         }
     }
 }
