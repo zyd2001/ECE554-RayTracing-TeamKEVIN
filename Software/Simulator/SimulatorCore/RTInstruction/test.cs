e00f81895e7f4ca64b42c50a58bbbd9e7f29cc17
zyd2001 <zyd200101@outlook.com>
2021-03-27 00:22:58 +0800

    RT-27 Scalar implicit conversion

---
+++ Software/Simulator/SimulatorCore/RTInstruction/test.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+
+namespace SimulatorCore
+{
+    class ii_sub : RTInstruction
+    {
+        int r0, r1, r2;
+
+        internal ii_sub(int r0, int r1, int r2)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.r2 = r2;
+        }
+
+        internal override void process(RegisterFile<Vector4> vRF, RegisterFile<Scalar> sRF, Memory mem, IntersectionCore IC)
+        {
+            sRF[r0] = sRF[r1].i + sRF[r2].i;
+        }
+    }
+}
