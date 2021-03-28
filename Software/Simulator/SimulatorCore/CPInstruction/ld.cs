da61b51ae304d337954ef92b902756bd4145e88f
Jordan Nunez <jmnunez@wisc.edu>
2021-03-27 19:49:43 -0500

    RT-27 CP Instructions up to ld

---
+++ Software/Simulator/SimulatorCore/CPInstruction/ld.cs
@@ -0,0 +1,22 @@
+namespace SimulatorCore
+{
+    class ld : CPInstruction
+    {
+        int r0, r1, imm;
+
+        internal ld(int r0, int r1, int imm)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.imm = imm;
+        }
+
+        internal override void process(RegisterFile<int> RF, Memory mem)
+        {
+            RF[r0] = mem.read(RF[r1] + imm);
+            
+        }
+    }
+
+
+}
---
+++ Software/Simulator/SimulatorCore/CPInstruction/sub.cs
@@ -0,0 +1,36 @@
+namespace SimulatorCore
+{
+    class sub : CPInstruction
+    {
+        int r0, r1, r2;
+
+        internal sub(int r0, int r1, int r2)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.r2 = r2;
+        }
+
+        internal override void process(RegisterFile<int> RF, Memory mem)
+        {
+            RF[r0] = RF[r1] - RF[r2];
+        }
+    }
+
+    class subi : CPInstruction
+    {
+        int r0, r1, imm;
+
+        internal subi(int r0, int r1, int imm)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.imm = imm;
+        }
+        
+        internal override void process(RegisterFile<int> RF, Memory mem)
+        {
+            RF[r0] = RF[r1] - imm;
+        }
+    }
+}
