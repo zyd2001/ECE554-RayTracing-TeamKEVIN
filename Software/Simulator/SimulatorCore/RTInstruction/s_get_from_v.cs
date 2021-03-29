38b5e1d3dda0c6208a02f9c11eaf17f6d30a7b75
Jordan Nunez <jmnunez@wisc.edu>
2021-03-28 23:04:18 -0500

    RT isa up to v_load16_byte

---
+++ Software/Simulator/SimulatorCore/RTInstruction/s_get_from_v.cs
@@ -0,0 +1,38 @@
+using System.Numerics;
+
+namespace SimulatorCore
+{
+    class s_get_from_v : RTInstruction
+    {
+        int r0, r1, index;
+
+        internal s_get_from_v(int r0, int r1, int index)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.index = index;
+        }
+
+        internal override void process(RegisterFile<Vector4> vRF, RegisterFile<Scalar> sRF, Memory mem, IntersectionCore ic)
+        {
+            
+
+            if(index == 0){
+                sRF[r0] = (int)vRF[r1].X;
+            }
+            else if(index == 1){
+                sRF[r0] = (int)vRF[r1].Y;
+            }
+            else if(index == 2){
+                sRF[r0] = (int)vRF[r1].Z;
+            }
+            else if(index == 3){
+                sRF[r0] = (int)vRF[r1].W;
+            }
+            
+            
+        }
+    }
+
+
+}
---
+++ Software/Simulator/SimulatorCore/RTInstruction/s_store_4byte.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Numerics;
+
+
+namespace SimulatorCore
+{
+    class s_store_4byte : RTInstruction
+    {
+        int r0, r1, imm;
+
+        internal s_store_4byte(int r0, int r1, int imm)
+        {
+            this.r0 = r0;
+            this.r1 = r1;
+            this.imm = imm;
+        }
+
+        internal override void process(RegisterFile<Vector4> vRF, RegisterFile<Scalar> sRF, Memory mem, IntersectionCore ic)
+        {
+            var bytes = BitConverter.GetBytes(sRF[r0].i);
+            mem.write(sRF[r1].i+imm, bytes);
+            
+            
+            
+        }
+    }
+
+
+}
