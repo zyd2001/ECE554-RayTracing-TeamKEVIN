995670c3ac70cad7b34357565556be21330f6336
Jialuo Gao <jialuogao980513@gmail.com>
2021-04-12 22:15:35 -0500

    rdy logic

--- Hardware/mem_main.sv
+++ Hardware/mem_main.sv
@@ -1,181 +1,252 @@
-module mem_main(clk, rst_n, we_RT, addr_RT, data_RT_in, addr_MC, re_MC,
+module mem_main(clk, rst_n, we_RT, re_RT, addr_RT, data_RT_in, addr_MC, re_MC,
                 data_RT_out, rdy_RT, data_MC_out, rdy_MC);
 
-  parameter NUM_RT = 4;
-  parameter NUM_THREAD = 64;
-  parameter NUM_BANK_PTHREAD = 4;
-  parameter NUM_BANK = NUM_THREAD * NUM_BANK_PTHREAD;
+    parameter NUM_RT = 4;
+    parameter NUM_THREAD = 64;
+    parameter NUM_BANK_PTHREAD = 4;
+    parameter NUM_BANK = NUM_THREAD * NUM_BANK_PTHREAD;
 
-  input clk, rst_n;
+    input clk, rst_n;
 
-  input we_RT[NUM_RT-1:0];
-  input [31:0] addr_RT[NUM_RT-1:0];
-  input [127:0] data_RT_in[NUM_RT-1:0];
+    input we_RT[NUM_RT-1:0];
+    input re_RT[NUM_RT-1:0];
+    input [31:0] addr_RT[NUM_RT-1:0];
+    input [127:0] data_RT_in[NUM_RT-1:0];
 
-  input re_MC;
-  input [31:0] addr_MC; //
+    input re_MC;
+    input [31:0] addr_MC; //
 
 
-  output rdy_RT[NUM_RT-1:0];//
-  output [127:0] data_RT_out[NUM_RT-1:0];
+    output logic rdy_RT[NUM_RT-1:0];//
+    output [127:0] data_RT_out[NUM_RT-1:0];
 
-  output rdy_MC;//
-  output [127:0] data_MC_out;
+    output logic rdy_MC;//
+    output [127:0] data_MC_out;
 
-  // reg [11:0] addr_bank[255:0];
-  genvar i, j, k;
-  // indicate if a group is hit with an input port address
-  wire [3:0] thread_hit[NUM_THREAD-1:0];
-  generate
-    for (i = 0; i < NUM_THREAD; i = i + 1) begin: hit_thread
-      for (j = 0; j < NUM_RT; j = j + 1) begin: port
-        assign thread_hit[i][j] = (i == addr_RT[j][21:16]);
-      end
+    localparam CYCLE_TO_FINISH = 4;
+    localparam FIN_COUNTER_BIT = $clog2(CYCLE_TO_FINISH);
+    genvar i, j;
+    
+    logic [FIN_COUNTER_BIT-1:0] fin_counter_RT [NUM_RT-1:0];
+    logic fin_counter_RT_en [NUM_RT-1:0];
+    logic [FIN_COUNTER_BIT-1:0] fin_counter_MC;
+    logic fin_counter_MC_en;
+    // ready logic for RT
+    generate
+        for (i = 0; i < NUM_RT; i++) begin
+            assign rdy_RT[i] = fin_counter_RT[i] === CYCLE_TO_FINISH;
+            always_ff @(posedge clk, negedge rst_n) begin
+                if (!rst_n)
+                    fin_counter_RT_en[i] <= 1'b0;
+                    fin_counter_RT[i] <= {FIN_COUNTER_BIT{1'b0}};
+                else if(rdy_RT[i]) begin
+                    fin_counter_RT_en[i] <= 1'b0;
+                    fin_counter_RT[i] <= {FIN_COUNTER_BIT{1'b0}};
+                end
+                else if(!fin_counter_RT_en[i]) begin
+                    fin_counter_RT_en[i] <= we_RT[i] | re_RT[i];
+                    fin_counter_RT[i] <= fin_counter_RT[i] + {(FIN_COUNTER_BIT-1){1'b0}, we_RT[i] | re_RT[i]};
+                end
+                else begin
+                    fin_counter_RT_en[i] <= fin_counter_RT_en;
+                    fin_counter_RT[i] <= fin_counter_RT[i] + 1;
+                end
+            end
+        end
+    endgenerate
+    // ready logic for MC
+    assign rdy_MC = fin_counter_MC === CYCLE_TO_FINISH;
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (!rst_n)
+            fin_counter_MC_en <= 1'b0;
+            fin_counter_MC <= {FIN_COUNTER_BIT{1'b0}};
+        else if(rdy_MC) begin
+            fin_counter_MC_en <= 1'b0;
+            fin_counter_MC <= {FIN_COUNTER_BIT{1'b0}};
+        end
+        else if(!fin_counter_MC_en) begin
+            fin_counter_MC_en <= re_MC;
+            fin_counter_MC <= fin_counter_MC[i] + {(FIN_COUNTER_BIT-1){1'b0}, re_MC};
+        end
+        else begin
+            fin_counter_MC_en <= fin_counter_MC_en;
+            fin_counter_MC <= fin_counter_MC[i] + 1;
+        end
     end
-  endgenerate
-  // write enable signal
-  wire we_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];
-  generate
-    for (i = 0; i < NUM_THREAD; i = i + 1) begin: thread
-      assign we_bank[i*4+3:i*4] = '{4{|(thread_hit[i] & {we_RT[3],we_RT[2],we_RT[1],we_RT[0]})}};
-    end
-  endgenerate
-  // address and data for each group
-  wire [11:0] address[NUM_THREAD-1:0];
-  reg [11:0] group_address[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
-  reg [31:0] group_data_in[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
-  wire [31:0] group_data_out[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
-  generate
-	for (i = 0; i < NUM_THREAD; i = i + 1) begin
-		assign address[i] = re_MC ? addr_MC :
-									  thread_hit[i][0] ? addr_RT[0][13:2] :
-                                   thread_hit[i][1] ? addr_RT[1][13:2] :
-                                   thread_hit[i][2] ? addr_RT[2][13:2] : addr_RT[3][13:2];
-	end
-  endgenerate
-  
-  generate
-    for (i = 0; i < NUM_THREAD; i = i + 1) begin: address_group
-		always_ff @(posedge clk, negedge rst_n) begin
-			if (!rst_n) begin
-				group_address[0][i] <= 12'b0;
-				group_address[1][i] <= 12'b0;
-				group_address[2][i] <= 12'b0;
-				group_address[3][i] <= 12'b0;
-			end
-			else begin
-				group_address[0][i] <= address[i];
-				group_address[1][i] <= address[i] + 12'h4;
-				group_address[2][i] <= address[i] + 12'h8;
-				group_address[3][i] <= address[i] + 12'hC;
-			end
-		end
-    end
-  endgenerate
-  generate
-	for (i = 0; i < NUM_THREAD; i = i + 1) begin
-		for (j = 0; j < NUM_BANK_PTHREAD; j = j + 1) begin
-			always_ff @(posedge clk, negedge rst_n) begin
-				if(!rst_n) 
-					group_data_in[j][i] <= 32'b0;
-				else begin
-					group_data_in[j][i] <= thread_hit[i][0] ? data_RT_in[0][j*32+31:j*32] :
-                                     thread_hit[i][1] ? data_RT_in[1][j*32+31:j*32] :
-                                     thread_hit[i][2] ? data_RT_in[2][j*32+31:j*32] : data_RT_in[3][j*32+31:j*32];
+
+
+    //Memory IO logic
+    logic we_bank[NUM_THREAD-1:0];
+    generate
+        for (i = 0; i < NUM_THREAD; i = i + 1) begin
+            always_ff @(posedge clk, negedge rst_n) begin
+                if (!rst_n)
+                    we_bank[i] <= 1'b0;
+                else begin
+                    we_bank[i] <= (addr_RT[0][21:16] == i) 
+                               || (addr_RT[1][21:16] == i) 
+                               || (addr_RT[2][21:16] == i) 
+                               || (addr_RT[3][21:16] == i); 
+                end
+            end
+        end
+    endgenerate
+
+    logic [11:0] addr_bank_0[NUM_RT-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_RT; i = i + 1) begin
+            always_ff @(posedge clk, negedge rst_n) begin
+                if (!rst_n) begin
+                    addr_bank_0[i][0] <= 12'b0;
+                    addr_bank_0[i][1] <= 12'b0;
+                    addr_bank_0[i][2] <= 12'b0;
+                    addr_bank_0[i][3] <= 12'b0;
+					 end
+                else begin
+                    addr_bank_0[i][0] <= addr_RT[i][3:2] == 2'h0 ? addr_RT[i][13:2]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h0 ? (addr_RT[i][13:2] + 12'h1)
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h0 ? (addr_RT[i][13:2] + 12'h2)
+                                        : (addr_RT[i][13:2] + 12'h3);
+                    addr_bank_0[i][1] <= addr_RT[i][3:2] == 2'h1 ? addr_RT[i][13:2]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h1 ? (addr_RT[i][13:2] + 12'h1)
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h1 ? (addr_RT[i][13:2] + 12'h2)
+                                        : (addr_RT[i][13:2] + 12'h3);      
+                    addr_bank_0[i][2] <= addr_RT[i][3:2] == 2'h2 ? addr_RT[i][13:2]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h2 ? (addr_RT[i][13:2] + 12'h1)
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h2 ? (addr_RT[i][13:2] + 12'h2)
+                                        : (addr_RT[i][13:2] + 12'h3);
+                    addr_bank_0[i][3] <= addr_RT[i][3:2] == 2'h3 ? addr_RT[i][13:2]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h3 ? (addr_RT[i][13:2] + 12'h1)
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h3 ? (addr_RT[i][13:2] + 12'h2)
+                                        : (addr_RT[i][13:2] + 12'h3);
+					end
+            end
+        end
+    endgenerate 
+
+    logic [32:0] data_bank_0[NUM_RT-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_RT; i = i + 1) begin
+            always_ff @(posedge clk, negedge rst_n) begin
+                if (!rst_n) begin
+                    data_bank_0[i][0] <= 32'b0;
+                    data_bank_0[i][1] <= 32'b0;
+                    data_bank_0[i][2] <= 32'b0;
+                    data_bank_0[i][3] <= 32'b0;
+					 end
+                else begin
+                    data_bank_0[i][0] <= addr_RT[i][3:2] == 2'h0 ? data_RT_in[i][31:0]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h0 ? data_RT_in[i][63:32]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h0 ? data_RT_in[i][95:64]
+                                        : data_RT_in[i][127:96];
+                    data_bank_0[i][1] <= addr_RT[i][3:2] == 2'h1 ? data_RT_in[i][31:0]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h1 ? data_RT_in[i][63:32]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h1 ? data_RT_in[i][95:64]
+                                        : data_RT_in[i][127:96];      
+                    data_bank_0[i][2] <= addr_RT[i][3:2] == 2'h2 ? data_RT_in[i][31:0]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h2 ? data_RT_in[i][63:32]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h2 ? data_RT_in[i][95:64]
+                                        : data_RT_in[i][127:96];
+                    data_bank_0[i][3] <= addr_RT[i][3:2] == 2'h3 ? data_RT_in[i][31:0]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h3 ? data_RT_in[i][63:32]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h3 ? data_RT_in[i][95:64]
+                                        : data_RT_in[i][127:96];
+					end
 				end
-			end
-		end
-	end
-  endgenerate
-  // output map
-  logic [31:0] din_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0], dout_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];;
-  logic [11:0] addr_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];
-  assign data_RT_out[0] = {dout_bank[4*addr_RT[0][21:16]],dout_bank[4*addr_RT[0][21:16]+1],
-                           dout_bank[4*addr_RT[0][21:16]+2],dout_bank[4*addr_RT[0][21:16]+3]};
-  assign data_RT_out[1] = {dout_bank[4*addr_RT[1][21:16]],dout_bank[4*addr_RT[1][21:16]+1],
-                           dout_bank[4*addr_RT[1][21:16]+2],dout_bank[4*addr_RT[1][21:16]+3]};
-  assign data_RT_out[2] = {dout_bank[4*addr_RT[2][21:16]],dout_bank[4*addr_RT[2][21:16]+1],
-                           dout_bank[4*addr_RT[2][21:16]+2],dout_bank[4*addr_RT[2][21:16]+3]};
-  assign data_RT_out[3] = {dout_bank[4*addr_RT[3][21:16]],dout_bank[4*addr_RT[3][21:16]+1],
-                           dout_bank[4*addr_RT[3][21:16]+2],dout_bank[4*addr_RT[3][21:16]+3]};
-  
-  // 4 to 4 mapping logic from group_address and group_data_in to din_bank and addr_bank
-  generate
-    for (i = 0; i < NUM_THREAD; i = i + 1) begin: hit_port_thread
-      always_comb begin
-        case(group_address[0][i][3:2])
-          2'b00: begin
-            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[0][i];
-            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[1][i];
-            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[2][i];
-            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[3][i];
-            addr_bank[i*NUM_BANK_PTHREAD] = group_address[0][i];
-            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[1][i];
-            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[2][i];
-            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[3][i];
-          end
-          2'b01: begin
-            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[0][i];
-            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[1][i];
-            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[2][i];
-            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[3][i];
-            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[0][i];
-            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[1][i];
-            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[2][i];
-            addr_bank[i*NUM_BANK_PTHREAD] = group_address[3][i];
-          end
-          2'b10: begin
-            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[0][i];
-            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[1][i];
-            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[2][i];
-            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[3][i];
-            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[0][i];
-            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[1][i];
-            addr_bank[i*NUM_BANK_PTHREAD] = group_address[2][i];
-            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[3][i];
-          end
-          2'b11: begin
-            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[0][i];
-            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[1][i];
-            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[2][i];
-            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[3][i];
-            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[0][i];
-            addr_bank[i*NUM_BANK_PTHREAD] = group_address[1][i];
-            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[2][i];
-            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[3][i];
-          end
-        endcase
-      end
-    end
-  endgenerate
+        end
+    endgenerate 
 
-  logic [31:0] din_bank_real[NUM_BANK-1:0];
-  logic [11:0] addr_bank_real[NUM_BANK-1:0];
-  generate
-    for (i = 0; i < NUM_BANK; i = i + 1) begin: memory_input
-      always_ff @(posedge clk, negedge rst_n ) begin 
-        if (!rst_n)
-          din_bank_real[i] <= 32'b0;
-        else 
-          din_bank_real[i] <= din_bank[i];
-      end
+    logic [11:0] addr_bank[NUM_THREAD-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_THREAD; i = i + 1) begin
+            for (j = 0; j < 4; j = j + 1) begin
+                always_ff @(posedge clk, negedge rst_n) begin
+                    if (!rst_n) 
+                        addr_bank[i][j] <= 12'b0;
+                    else begin
+                        addr_bank[i][j] <= (i == addr_RT[0][21:16]) ? addr_bank_0[0][j]
+                                    : (i == addr_RT[1][21:16]) ? addr_bank_0[1][j]
+                                    : (i == addr_RT[2][21:16]) ? addr_bank_0[2][j] 
+                                    : addr_bank_0[3][j];
+                    end
+                end 
+//						assign addr_bank[i][j] = (i == addr_RT[0][21:16]) ? addr_bank_0[0][j]
+//                                    : (i == addr_RT[1][21:16]) ? addr_bank_0[1][j]
+//                                    : (i == addr_RT[2][21:16]) ? addr_bank_0[2][j] 
+//                                    : addr_bank_0[3][j];
+            end
+        end
+    endgenerate
 
-      always_ff @(posedge clk, negedge rst_n ) begin 
-        if (!rst_n)
-          addr_bank_real[i] <= 12'b0;
-        else 
-          addr_bank_real[i] <= addr_bank[i];
-      end
-    end 
-  endgenerate
-  
+    logic [31:0] data_bank[NUM_THREAD-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_THREAD; i = i + 1) begin
+            for (j = 0; j < 4; j = j + 1) begin
+                always_ff @(posedge clk, negedge rst_n) begin
+                    if (!rst_n) 
+                        data_bank[i][j] <= 32'b0;
+                    else begin
+                        data_bank[i][j] <= (i == addr_RT[0][21:16]) ? data_bank_0[0][j]
+                                    : (i == addr_RT[1][21:16]) ? data_bank_0[1][j]
+                                    : (i == addr_RT[2][21:16]) ? data_bank_0[2][j] 
+                                    : data_bank_0[3][j];
+                    end
+                end
+//						assign data_bank[i][j] = (i == addr_RT[0][21:16]) ? data_bank_0[0][j]
+//                                    : (i == addr_RT[1][21:16]) ? data_bank_0[1][j]
+//                                    : (i == addr_RT[2][21:16]) ? data_bank_0[2][j] 
+//                                    : data_bank_0[3][j];
+            end
+        end
+    endgenerate
 
-  genvar a;
-  generate
-    for (a = 0; a < NUM_BANK; a = a + 1) begin: main_memory
-      single_port_ram #(.ADDR_WIDTH(12), .DATA_WIDTH(32)) bank(.clk(clk), .we(we_bank[a]),
-            .data(din_bank_real[a]),.addr(addr_bank_real[a]), .q(dout_bank[a]));
-    end
-  endgenerate
+    logic [31:0] q_bank[NUM_THREAD-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_THREAD; i = i + 1) begin: main_memory_thread
+            for (j = 0; j < 4; j = j + 1) begin: main_memory_bank
+                single_port_ram #(.ADDR_WIDTH(12), .DATA_WIDTH(32)) bank(.clk(clk), .we(we_bank[i]),
+                .data(data_bank[i][j]),.addr(addr_bank[i][j]), .q(q_bank[i][j]));
+            end
+        end
+    endgenerate
 
-endmodule
+    logic [31:0] data_RT_out_0[NUM_RT-1:0][3:0];
+    generate
+        for (i = 0; i < NUM_RT; i = i + 1) begin
+            for (j = 0; j < 4; j = j + 1) begin
+					always_ff @(posedge clk, negedge rst_n) begin
+						if (!rst_n) 
+                    data_RT_out_0[i][j] <= 32'b0;
+						else  
+                    data_RT_out_0[i][j] <= q_bank[addr_RT[i][21:16]][j];
+					end
+            end   
+        end
+    endgenerate 
+
+    generate
+        for (i = 0; i < NUM_RT; i = i + 1) begin
+            assign data_RT_out[i][31:0] = addr_RT[i][3:2] == 2'h0 ? data_RT_out_0[i][0]
+                                        : addr_RT[i][3:2] == 2'h1 ? data_RT_out_0[i][1]
+                                        : addr_RT[i][3:2] == 2'h2 ? data_RT_out_0[i][2]
+                                        : data_RT_out_0[i][3];
+            
+            assign data_RT_out[i][63:32] = (addr_RT[i][3:2] + 2'h1) == 2'h0 ? data_RT_out_0[i][0]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h1 ? data_RT_out_0[i][1]
+                                        : (addr_RT[i][3:2] + 2'h1) == 2'h2 ? data_RT_out_0[i][2]
+                                        : data_RT_out_0[i][3];
+
+            assign data_RT_out[i][95:64] = (addr_RT[i][3:2] + 2'h2) == 2'h0 ? data_RT_out_0[i][0]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h1 ? data_RT_out_0[i][1]
+                                        : (addr_RT[i][3:2] + 2'h2) == 2'h2 ? data_RT_out_0[i][2]
+                                        : data_RT_out_0[i][3];
+                                        
+            assign data_RT_out[i][127:96] = (addr_RT[i][3:2] + 2'h3) == 2'h0 ? data_RT_out_0[i][0]
+                                        : (addr_RT[i][3:2] + 2'h3) == 2'h1 ? data_RT_out_0[i][1]
+                                        : (addr_RT[i][3:2] + 2'h3) == 2'h2 ? data_RT_out_0[i][2]
+                                        : data_RT_out_0[i][3];
+            
+        end
+    endgenerate
+
+endmodule 
---
+++ Hardware/mem_main_old.sv
@@ -0,0 +1,181 @@
+module mem_main_old(clk, rst_n, we_RT, addr_RT, data_RT_in, addr_MC, re_MC,
+                data_RT_out, rdy_RT, data_MC_out, rdy_MC);
+
+  parameter NUM_RT = 4;
+  parameter NUM_THREAD = 64;
+  parameter NUM_BANK_PTHREAD = 4;
+  parameter NUM_BANK = NUM_THREAD * NUM_BANK_PTHREAD;
+
+  input clk, rst_n;
+
+  input we_RT[NUM_RT-1:0];
+  input [31:0] addr_RT[NUM_RT-1:0];
+  input [127:0] data_RT_in[NUM_RT-1:0];
+
+  input re_MC;
+  input [31:0] addr_MC; //
+
+
+  output rdy_RT[NUM_RT-1:0];//
+  output [127:0] data_RT_out[NUM_RT-1:0];
+
+  output rdy_MC;//
+  output [127:0] data_MC_out;
+
+  // reg [11:0] addr_bank[255:0];
+  genvar i, j, k;
+  // indicate if a group is hit with an input port address
+  wire [3:0] thread_hit[NUM_THREAD-1:0];
+  generate
+    for (i = 0; i < NUM_THREAD; i = i + 1) begin: hit_thread
+      for (j = 0; j < NUM_RT; j = j + 1) begin: port
+        assign thread_hit[i][j] = (i == addr_RT[j][21:16]);
+      end
+    end
+  endgenerate
+  // write enable signal
+  wire we_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];
+  generate
+    for (i = 0; i < NUM_THREAD; i = i + 1) begin: thread
+      assign we_bank[i*4+3:i*4] = '{4{|(thread_hit[i] & {we_RT[3],we_RT[2],we_RT[1],we_RT[0]})}};
+    end
+  endgenerate
+  // address and data for each group
+  wire [11:0] address[NUM_THREAD-1:0];
+  reg [11:0] group_address[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
+  reg [31:0] group_data_in[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
+  wire [31:0] group_data_out[NUM_BANK_PTHREAD][NUM_THREAD-1:0];
+  generate
+	for (i = 0; i < NUM_THREAD; i = i + 1) begin
+		assign address[i] = re_MC ? addr_MC :
+									  thread_hit[i][0] ? addr_RT[0][13:2] :
+                                   thread_hit[i][1] ? addr_RT[1][13:2] :
+                                   thread_hit[i][2] ? addr_RT[2][13:2] : addr_RT[3][13:2];
+	end
+  endgenerate
+  
+  generate
+    for (i = 0; i < NUM_THREAD; i = i + 1) begin: address_group
+		always_ff @(posedge clk, negedge rst_n) begin
+			if (!rst_n) begin
+				group_address[0][i] <= 12'b0;
+				group_address[1][i] <= 12'b0;
+				group_address[2][i] <= 12'b0;
+				group_address[3][i] <= 12'b0;
+			end
+			else begin
+				group_address[0][i] <= address[i];
+				group_address[1][i] <= address[i] + 12'h4;
+				group_address[2][i] <= address[i] + 12'h8;
+				group_address[3][i] <= address[i] + 12'hC;
+			end
+		end
+    end
+  endgenerate
+  generate
+	for (i = 0; i < NUM_THREAD; i = i + 1) begin
+		for (j = 0; j < NUM_BANK_PTHREAD; j = j + 1) begin
+			always_ff @(posedge clk, negedge rst_n) begin
+				if(!rst_n) 
+					group_data_in[j][i] <= 32'b0;
+				else begin
+					group_data_in[j][i] <= thread_hit[i][0] ? data_RT_in[0][j*32+31:j*32] :
+                                     thread_hit[i][1] ? data_RT_in[1][j*32+31:j*32] :
+                                     thread_hit[i][2] ? data_RT_in[2][j*32+31:j*32] : data_RT_in[3][j*32+31:j*32];
+				end
+			end
+		end
+	end
+  endgenerate
+  // output map
+  logic [31:0] din_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0], dout_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];;
+  logic [11:0] addr_bank[NUM_BANK_PTHREAD*NUM_THREAD-1:0];
+  assign data_RT_out[0] = {dout_bank[4*addr_RT[0][21:16]],dout_bank[4*addr_RT[0][21:16]+1],
+                           dout_bank[4*addr_RT[0][21:16]+2],dout_bank[4*addr_RT[0][21:16]+3]};
+  assign data_RT_out[1] = {dout_bank[4*addr_RT[1][21:16]],dout_bank[4*addr_RT[1][21:16]+1],
+                           dout_bank[4*addr_RT[1][21:16]+2],dout_bank[4*addr_RT[1][21:16]+3]};
+  assign data_RT_out[2] = {dout_bank[4*addr_RT[2][21:16]],dout_bank[4*addr_RT[2][21:16]+1],
+                           dout_bank[4*addr_RT[2][21:16]+2],dout_bank[4*addr_RT[2][21:16]+3]};
+  assign data_RT_out[3] = {dout_bank[4*addr_RT[3][21:16]],dout_bank[4*addr_RT[3][21:16]+1],
+                           dout_bank[4*addr_RT[3][21:16]+2],dout_bank[4*addr_RT[3][21:16]+3]};
+  
+  // 4 to 4 mapping logic from group_address and group_data_in to din_bank and addr_bank
+  generate
+    for (i = 0; i < NUM_THREAD; i = i + 1) begin: hit_port_thread
+      always_comb begin
+        case(group_address[0][i][3:2])
+          2'b00: begin
+            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[0][i];
+            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[1][i];
+            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[2][i];
+            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[3][i];
+            addr_bank[i*NUM_BANK_PTHREAD] = group_address[0][i];
+            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[1][i];
+            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[2][i];
+            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[3][i];
+          end
+          2'b01: begin
+            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[0][i];
+            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[1][i];
+            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[2][i];
+            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[3][i];
+            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[0][i];
+            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[1][i];
+            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[2][i];
+            addr_bank[i*NUM_BANK_PTHREAD] = group_address[3][i];
+          end
+          2'b10: begin
+            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[0][i];
+            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[1][i];
+            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[2][i];
+            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[3][i];
+            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[0][i];
+            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[1][i];
+            addr_bank[i*NUM_BANK_PTHREAD] = group_address[2][i];
+            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[3][i];
+          end
+          2'b11: begin
+            din_bank[i*NUM_BANK_PTHREAD+3] = group_data_in[0][i];
+            din_bank[i*NUM_BANK_PTHREAD] = group_data_in[1][i];
+            din_bank[i*NUM_BANK_PTHREAD+1] = group_data_in[2][i];
+            din_bank[i*NUM_BANK_PTHREAD+2] = group_data_in[3][i];
+            addr_bank[i*NUM_BANK_PTHREAD+3] = group_address[0][i];
+            addr_bank[i*NUM_BANK_PTHREAD] = group_address[1][i];
+            addr_bank[i*NUM_BANK_PTHREAD+1] = group_address[2][i];
+            addr_bank[i*NUM_BANK_PTHREAD+2] = group_address[3][i];
+          end
+        endcase
+      end
+    end
+  endgenerate
+
+  logic [31:0] din_bank_real[NUM_BANK-1:0];
+  logic [11:0] addr_bank_real[NUM_BANK-1:0];
+  generate
+    for (i = 0; i < NUM_BANK; i = i + 1) begin: memory_input
+      always_ff @(posedge clk, negedge rst_n ) begin 
+        if (!rst_n)
+          din_bank_real[i] <= 32'b0;
+        else 
+          din_bank_real[i] <= din_bank[i];
+      end
+
+      always_ff @(posedge clk, negedge rst_n ) begin 
+        if (!rst_n)
+          addr_bank_real[i] <= 12'b0;
+        else 
+          addr_bank_real[i] <= addr_bank[i];
+      end
+    end 
+  endgenerate
+  
+
+  genvar a;
+  generate
+    for (a = 0; a < NUM_BANK; a = a + 1) begin: main_memory
+      single_port_ram #(.ADDR_WIDTH(12), .DATA_WIDTH(32)) bank(.clk(clk), .we(we_bank[a]),
+            .data(din_bank_real[a]),.addr(addr_bank_real[a]), .q(dout_bank[a]));
+    end
+  endgenerate
+
+endmodule
