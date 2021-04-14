10f82cea35dd35b89a5a14b5b14286e22e216a33
zyd2001 <zyd200101@outlook.com>
2021-04-14 09:17:12 +0800

    RT-27 specify output file

--- Software/Simulator/SimulatorCLI/Program.cs
+++ Software/Simulator/SimulatorCLI/Program.cs
@@ -15,15 +15,17 @@ namespace SimulatorCLI
         /// <param name="CPMSize">CP memory size (in byte)</param>
         /// <param name="RTMSize">RT Core main memory size (in byte)</param>
         /// <param name="Triangle">Traingle binary file</param>
+        /// <param name="RTO">RT Trace Output file (Fallback will be Console)</param>
+        /// <param name="CPO">CP Trace Output file (Fallback will be Console)</param>
         static void Main(FileInfo CP = null, FileInfo RT = null, bool DT = false, int CPMSize = 4096, int RTMSize = 4194304,
-            FileInfo Triangle = null)
+            FileInfo Triangle = null, FileInfo RTO = null, FileInfo CPO = null)
         {
             if (CP == null && RT == null)
             {
                 Console.WriteLine("Must specify one binary file path for RT or CP");
                 return;
             }
-            Simulator sim = new Simulator(CP, RT, DT, CPMSize, RTMSize, Triangle);
+            Simulator sim = new Simulator(CP, RT, DT, CPMSize, RTMSize, Triangle, CPO, RTO);
             if (CP != null)
                 sim.run();
             else
