d429bf1e7db79cc4511424347a2977e8aa72c031
johnw08 <ywang989@wisc.edu>
2021-04-16 18:31:40 -0500

    hardware_dev modified main mem interface

--- Hardware/mem_main.sv
+++ Hardware/mem_main.sv
@@ -1,25 +1,30 @@
-module mem_main(clk, rst_n, we_RT, re_RT, addr_RT, data_RT_in, addr_MC, re_MC,
-                data_RT_out, rdy_RT, data_MC_out, rdy_MC);
+module mem_main(clk, rst_n, we_RT, re_RT, addr_RT, data_RT_in, re_MC,
+                data_RT_out, rd_rdy_RT, data_MC_out, rdy_MC);
 
     parameter NUM_RT = 4;
     parameter NUM_THREAD = 64;
     parameter NUM_BANK_PTHREAD = 4;
-    parameter NUM_BANK = NUM_THREAD * NUM_BANK_PTHREAD;
+    localparam NUM_BANK = NUM_THREAD * NUM_BANK_PTHREAD;
 
+    /*
+        Input
+    */
     input clk, rst_n;
-
+    //RT
     input we_RT[NUM_RT-1:0];
     input re_RT[NUM_RT-1:0];
     input [31:0] addr_RT[NUM_RT-1:0];
     input [127:0] data_RT_in[NUM_RT-1:0];
-
+    //MC
     input re_MC;
-    input [31:0] addr_MC;
 
-
-    output rdy_RT[NUM_RT-1:0];
+    /*
+        Output
+    */
+    //RT
+    output reg rd_rdy_RT[NUM_RT-1:0];
     output [127:0] data_RT_out[NUM_RT-1:0];
-
-    output rdy_MC;
+    //MC
+    output reg rdy_MC;
     output [127:0] data_MC_out;
 endmodule
