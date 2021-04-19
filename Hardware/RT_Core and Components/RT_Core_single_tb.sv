67d43d40a39689a91c9c400e63601cd31fcfff98
Jordan Nunez <jordanmnunez16@gmail.com>
2021-04-19 18:07:22 -0500

    RT-43 read file

--- Hardware/RT_Core and Components/RT_Core_single_tb.sv
+++ Hardware/RT_Core and Components/RT_Core_single_tb.sv
@@ -1,9 +1,16 @@
 //import rand_assembly.out;
+import "DPI-C" function string getenv(input string env_name);
 module RT_Core_single_tb;
-	
+	int file;
+	string filename;
+	logic [31:0] MRTI [0:4095];
+	int i;
+
 	initial begin
-		reg [31:0] MRTI [0:4095];
-		$readmemb("rand_assembly.out", MRTI);
+		
+		filename = {getenv("PWD"), "/rand_assembly.out"};
+		file = $fopen(filename, "rb");
+		i = $fread(MRTI, file);
 	
 	end
 endmodule
