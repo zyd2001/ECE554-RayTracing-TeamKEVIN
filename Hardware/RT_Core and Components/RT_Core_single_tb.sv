417f9389186de0d5f70f421222d2af2ad729de10
Jordan Nunez <jordanmnunez16@gmail.com>
2021-04-19 18:40:11 -0500

    RT-43 Read Instructions into Memory and Bit fix
    reading the binary into memory, the bits were opposite of what they should have been, so they had to be notted.

--- Hardware/RT_Core and Components/RT_Core_single_tb.sv
+++ Hardware/RT_Core and Components/RT_Core_single_tb.sv
@@ -4,13 +4,18 @@ module RT_Core_single_tb;
 	int file;
 	string filename;
 	logic [31:0] MRTI [0:4095];
-	int i;
+	logic [31:0] not_MRTI [0:4095];
+	int status;
 
 	initial begin
 		
 		filename = {getenv("PWD"), "/rand_assembly.out"};
 		file = $fopen(filename, "rb");
-		i = $fread(MRTI, file);
+		status = $fread(not_MRTI, file);
+		
 	
 	end
+
+	always_comb foreach (not_MRTI[i]) MRTI[i] = ~not_MRTI[i];
+	
 endmodule
