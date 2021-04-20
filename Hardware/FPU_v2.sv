6642a835da3f64490234ed02fe2fd46cf1120454
DiKev <cevinding1997@gmail.com>
2021-04-20 11:24:04 +0800

    testbench

--- Hardware/FPU_v2.sv
+++ Hardware/FPU_v2.sv
@@ -1,29 +1,21 @@
 module FALU (
-    clk, rst, en
+    clk, rst, en,
     mode, 
     a,
     b,
     done,
     out);
 
-  input clk;
-  input rst;
+  input clk, rst, en;
   input [2:0] mode;
   input [31:0] a, b;
   
   output done;
   output [31:0] out;
   
-  wire [31:0] add_result, mul_result, sub_result;
-  
-  reg [3:0] enable;
-  reg [3:0] module_rst;
-  reg [31:0] out_r;
-  
-  assign enable = 1 << mode;
   assign module_rst = rst << mode;
 
-  FPU u0 (
+  fpoint2_multi u0 (
 		.clk       (clk),      //   input,   width = 1, s1.clk
 		.clk_en    (en),       //   input,   width = 1,   .clk_en
 		.dataa     (a),        //   input,  width = 32,   .dataa
@@ -31,7 +23,7 @@ module FALU (
 		.n         (mode),     //   input,   width = 3,   .n
 		.reset     (rst),      //   input,   width = 1,   .reset
 		.reset_req (rst),      //   input,   width = 1,   .reset_req
-		.start     (start),    //   input,   width = 1,   .start
+		.start     (en),    //   input,   width = 1,   .start
 		.done      (done),     //  output,   width = 1,   .done
 		.result    (out)       //  output,  width = 32,   .result
 	);
