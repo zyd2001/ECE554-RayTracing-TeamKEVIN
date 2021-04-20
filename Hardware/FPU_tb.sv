2516883e060a8016b93c9c12c3d91bacd932afb3
DiKev <cevinding1997@gmail.com>
2021-04-20 11:24:04 +0800

    testbench

--- Hardware/FPU_tb.sv
+++ Hardware/FPU_tb.sv
@@ -2,26 +2,29 @@
 
 module FPU_tb();
   
-  reg clk, rst;
-  reg [1:0] mode;
+  reg clk, rst, en, done;
+  reg [2:0] mode;
   reg [31:0] a, b, out;
   
-  FPU iDUT(
-    .clk(clk), .rst(rst),
+  FALU iDUT(
+    .clk(clk), .rst(rst), .en(en),
     .mode(mode), 
     .a(a),
     .b(b),
+    .done(done),
     .out(out));
   
   initial begin
     clk = 1'b1;
-    rst = 1'b0;
+    rst = 1'b1;
+    en = 1'b0;
     a = 32'b0_10000001_00001100110011001100110;  // 4.2 
     b = 32'b0_10000000_10011001100110011001100;  // 3.2
-    mode = 2'b00;
+    mode = 3'b100;
     #10
-    rst = 1'b1;
-    
+    rst = 1'b0;
+    #20
+    en = 1'b1;
   end
   
   always #5 clk = ~clk;
