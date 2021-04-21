d69b3b8d1acec1e4035aac7ee76e3d709951677e
DiKev <cevinding1997@gmail.com>
2021-04-21 20:37:51 +0800

    ALU v2

--- Hardware/ALU.sv
+++ Hardware/ALU.sv
@@ -1,73 +1,47 @@
 module ALU (
   clk, rst,
-  Fix_mode, Fix_a, Fix_b, Fix_result, Fix_start, Fix_done,
-  Float_mode, Float_a, Float_b, Float_result, Float_start, Float_done,
-  Converter_mode, Converter_in, Converter_out, Converter_start, Converter_done);
+  Fix_mode, Fix_en, Fix_a, Fix_b, Fix_done, Fix_result,
+  Float_mode, Float_en, Float_a, Float_b, Float_done, Float_result, 
+  Converter_mode, Converter_en, Converter_in, Converter_done, Converter_out,
+  Sqrt_in, Sqrt_en, Sqrt_done, Sqrt_out);
   
   input clk, rst;
+  input Fix_en, Float_en, Converter_en, Sqrt_en;
   input Converter_mode;
-  input Fix_start, Float_start, Converter_start;
-  input [2:0] Fix_mode, Float_mode;
-  input [31:0] Fix_a, Fix_b, Float_a, Float_b, Converter_in;
+  input [1:0] Fix_mode, Float_mode;
+  input [31:0] Fix_a, Fix_b, Float_a, Float_b, Converter_in, Sqrt_in;
   
-  output Fix_done, Float_done, Converter_done;
-  output [31:0] Float_result, Converter_out;
+  output Fix_done, Float_done, Converter_done, Sqrt_out;
+  output [31:0] Float_result, Converter_out, Sqrt_out;
   output [63:0] Fix_result;
-  
-  logic [31:0] Float_check, Float_check_in, Converter_check, Converter_check_in;
-  logic [63:0] Fix_check, Fix_check_in;
-  
-  logic [5:0] Fix_counter, Fix_counter_in, Float_counter, Float_counter_in, Converter_counter, Converter_counter_in;
-  
-  always@(posedge clk or posedge rst) begin
-    if (rst) begin
-      Fix_check <= '0;
-      Float_check <= '0;
-      Converter_check <= '0;
-      Fix_counter <= '0;
-      Float_counter <= '0;
-      Converter_counter <= '0;
-    end
-    else begin
-      Fix_check <= Fix_check_in ;    
-      Float_check <= Float_check_in;
-      Converter_check <= Converter_check_in;
-      Fix_counter <= Fix_counter_in;
-      Float_counter <= Float_counter_in;
-      Converter_counter <= Converter_counter_in;
-    end
-  end
-    
-  assign Fix_check_in = Fix_start ? Fix_result : Fix_check;
-  assign Float_check_in = Float_start ? Float_result : Fix_check;
-  assign Converter_check_in = Converter_start ? Converter_out : Converter_check;
-  
-  assign Fix_counter_in = ((Fix_counter == 6'd50) || (Fix_check !== Fix_result)) ? 5'h0 : (Fix_counter + 1);
-  assign Float_counter_in = ((Float_counter == 6'd50) || (Float_check !== Float_result)) ? 5'h0 : (Float_counter + 1);
-  assign Converter_counter_in = ((Converter_counter == 6'd50) || (Converter_check !== Converter_out)) ? 5'h0 : (Converter_counter + 1);
-  
-  assign Fix_done = ((Fix_counter == 6'd50) || (Fix_check !== Fix_result)) ? 1'b1 : 1'b0;
-  assign Float_done = ((Float_counter == 6'd50) || (Float_check !== Float_result)) ? 1'b1 : 1'b0;
-  assign Converter_done = ((Converter_counter == 6'd50) || (Converter_check !== Converter_out)) ? 1'b1 : 1'b0;
-  
-  FixPU (
-    .clk(clk), .rst(Fix_rst),
+
+  FixPU Fix(
+    .clk(clk), .rst(Fix_rst), .en(Fix_en),
     .mode(Fix_mode),
     .a(Fix_a),
     .b(Fix_b),
+    .done(Fix_done),
     .result(Fix_result));
   
-  FloatPU (
-    .clk(clk), .rst(Float_rst),
+  FloatPU Float(
+    .clk(clk), .rst(Float_rst), .en(Float_en),
     .mode(Float_mode),
     .a(Float_a),
     .b(Float_b),
+    .done(Float_done),
     .result(Float_result));
 
-  Converter (
-    .clk(clk), .rst(Converter_rst),
+  Converter Convert(
+    .clk(clk), .rst(Converter_rst), .en(Converter_en), 
     .mode(Converter_mode),
     .data_in(Converter_in),
+    .done(Converter_done),
     .data_out(Converter_out));
+  
+  Sqrter Sqrt(
+    .clk(clk), .rst(Sqrt_rst), .en(Sqrt_en),
+    .in(Sqrt_en),
+    .done(Sqrt_done),
+    .out(Sqrt_out));
   
 endmodule
--- Hardware/FixPU.sv
+++ Hardware/FixPU.sv
@@ -1,62 +1,136 @@
 module FixPU (
-  clk, rst,
+  clk, rst, en,
   mode,
   a,
   b,
+  done,
   result);
   
-  input clk, rst;
-  input [2:0] mode;
+  parameter ADD_LATENCY = 3;
+            MUL_LATENCY = 8;
+            DIV_LATENCY = 40;
+            
+  input clk, rst, en;
+  input [1:0] mode;
   input [31:0] a, b;
   
+  output done;
   output [63:0] result;
   
-  logic Adder_en, Multiplier_en, Divider_en, Sqrter_en;
-  logic Adder_rst, Multiplier_rst, Divider_rst, Sqrter_rst;
- 
+  logic done_in;
+  
+  logic Adder_en, Multiplier_en, Divider_en;
+  
+  logic [5:0] counter, counter_in;
+  
   logic [31:0] Adder_b;
   
-  logic [31:0] Divider_result_in, Sqrter_result_in;
+  logic [31:0] Divider_result_in;
   logic [32:0] Adder_result_in;
   logic [63:0] Multiplier_result_in;
-
-  logic [63:0] Adder_result, Multiplier_result, Divider_result, Sqrter_result;
+  logic [63:0] result_in;
   
-  assign Adder_en = (mode[2:1] == 2'b00) ? 1'b1 : 1'b0;
-  assign Multiplier_en = (mode == 3'b010) ? 1'b1 : 1'b0;
-  assign Divider_en = (mode == 3'b011) ? 1'b1 : 1'b0;
-  assign Sqrter_en = (mode[2] == 1'b1) ? 1'b1 : 1'b0;
+  logic [63:0] Adder_result, Multiplier_result, Divider_result;
   
-  assign Adder_rst = rst && Adder_en;
-  assign Multiplier_rst = rst && Multiplier_en;
-  assign Divider_rst = rst && Divider_en;
-  assign Sqrter_rst = rst && Sqrter_en;
+  typedef enum [1:0] reg {ADDSUB, MUL, DIV, IDLE} state_t;
+  state_t state, nxt_state;
   
   assign Adder_b = mode[0] ? (~b + 1) : b;
   
-  assign result = (mode == 3'b010) ? Multiplier_result :
-                  (mode == 3'b011) ? Divider_result :
-                  (mode[2] == 1'b1) ? Sqrter_result : Adder_result;
-                  
   always@(posedge clk or posedge rst) begin
     if (rst) begin
       Adder_result <= '0;
       Multiplier_result <= '0;
       Divider_result <= '0;
-      Sqrter_result <= '0;
     end
     else begin
       Adder_result <= {31'h0, Adder_result_in};
       Multiplier_result <= Multiplier_result_in;
       Divider_result <= {32'h0, Divider_result_in};
-      Sqrter_result <= {32'h0, Sqrter_result_in};
     end
   end  
   
+  always_ff@(posedge clk or posedge rst) begin
+    if (rst) begin
+      counter <= '0;
+      state <= IDLE;
+      done <= '0;
+      result <= '0;
+    end
+    else begin
+      counter <= counter_in;
+      state <= nxt_state;
+      done <= done_in;
+      result <= result_in;
+    end
+  end
+  
+  always_comb begin
+    Adder_en = 1'b0;
+    Multiplier_en = 1'b0;
+    Divider_en = 1'b0;
+    done_in = 1'b0;
+    counter_in = '0;
+    result_in = result;
+    nxt_state = IDLE;
+    case(state)
+      ADDSUB: 
+        begin
+          if (counter == ADD_LATENCY) begin
+            done_in = 1'b1;
+            result_in = Adder_result;
+          end
+          else begin
+            counter_in = counter + 1'b1;
+            Adder_en = 1'b1;
+            nxt_state = ADDSUB;
+          end
+        end
+      MUL: 
+        begin
+          if (counter == MUL_LATENCY) begin
+            done_in = 1'b1;
+            result_in = Multiplier_result;
+          end
+          else begin
+            counter_in = counter + 1'b1;
+            Multiplier_en = 1'b1;
+            nxt_state = MUL;
+          end
+        end
+      DIV: 
+        begin
+          if (counter == DIV_LATENCY) begin
+            done_in = 1'b1;
+            result_in = Divider_result;
+          end
+          else begin
+            counter_in = counter + 1'b1;
+            Divider_en = 1'b1;
+            nxt_state = DIV;
+          end
+        end
+      default: 
+          if (en) begin
+            if (mode[1] == 1'b0) begin
+              Adder_en = 1'b1;
+              nxt_state = ADDSUB;
+            end
+            else if (mode == 2'b10) begin
+              Multiplier_en = 1'b1;
+              nxt_state = MUL;
+            end
+            else begin
+              Divider_en = 1'b1;
+              nxt_state = DIV;
+            end
+          end
+    endcase
+  end
   
   Fix_Add Adder (
 		.clk    (clk),          //   input,   width = 1,    clk.clk
-		.rst    (Adder_rst),    //   input,   width = 1,    rst.reset
+		.rst    (rst),    //   input,   width = 1,    rst.reset
 		.en     (Adder_en),     //   input,   width = 1,     en.en
 		.a0     (a),            //   input,  width = 32,     a0.a0
 		.a1     (Adder_b),      //   input,  width = 32,     a1.a1
@@ -65,7 +139,7 @@ module FixPU (
 
 	Fix_Mul Multiplier (
 		.clk    (clk),               //   input,   width = 1,    clk.clk
-		.rst    (Multiplier_rst),    //   input,   width = 1,    rst.reset
+		.rst    (rst),    //   input,   width = 1,    rst.reset
 		.en     (Multiplier_en),     //   input,   width = 1,     en.en
 		.a      (a),                 //   input,  width = 32,      a.a
 		.b      (b),                 //   input,  width = 32,      b.b
@@ -74,19 +148,11 @@ module FixPU (
 
 	Fix_Div Divider (
 		.clk         (clk),                 //   input,   width = 1,         clk.clk
-		.rst         (Divider_rst),         //   input,   width = 1,         rst.reset
+		.rst         (rst),         //   input,   width = 1,         rst.reset
 		.en          (Divider_en),          //   input,   width = 1,          en.en
 		.numerator   (a),                   //   input,  width = 32,   numerator.numerator
 		.denominator (b),                   //   input,  width = 32, denominator.denominator
 		.result      (Divider_result_in)       //  output,  width = 32,      result.result
 	);
   
-	Fix_Sqrt Sqrter (
-		.clk     (clk),            //   input,   width = 1,     clk.clk
-		.rst     (Sqrter_rst),     //   input,   width = 1,     rst.reset
-		.en      (Sqrter_en),      //   input,   width = 1,      en.en
-		.radical (a),              //   input,  width = 32, radical.radical
-		.result  (Sqrter_result_in)   //  output,  width = 32,  result.result
-	);
-
 endmodule
