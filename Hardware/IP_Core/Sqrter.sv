a04214476aa06468e8b631c775b13d7211bc90d9
DiKev <cevinding1997@gmail.com>
2021-04-22 18:26:44 +0800

    IP v3

--- Hardware/IP_Core/Sqrter.sv
+++ Hardware/IP_Core/Sqrter.sv
@@ -1,12 +1,12 @@
 module Sqrter (
-  clk, rst, en,
+  clk, rst, en, start,
   in,
   done,
   out);
   
   parameter LATENCY = 19;
   
-  input clk, rst, en;
+  input clk, rst, en, start;
   input [31:0] in;
   
   output done;
@@ -14,6 +14,8 @@ module Sqrter (
   
   logic done_in, done_reg;
   logic [4:0] counter, counter_in;
+  
+  logic [31:0] Sqrt_out, result_in, result_reg;
   
   typedef enum reg {COUNT, IDLE} state_t;
   state_t state, nxt_state;
@@ -23,22 +25,26 @@ module Sqrter (
       counter <= '0;
       state <= IDLE;
       done_reg <= '0;
+      result_reg <= '0;
     end
     else begin
       counter <= counter_in;
       state <= nxt_state;
       done_reg <= done_in;
+      result_reg <= result_in;
     end
   end
   
   always_comb begin
     counter_in = '0;
     done_in = 1'b0;
+    result_in = result_reg;
     nxt_state = IDLE;
     case(state)
       COUNT: 
         begin
           if (counter == LATENCY) begin
+            result_in = Sqrt_out;
             done_in = 1'b1;
           end
           else begin
@@ -47,7 +53,7 @@ module Sqrter (
           end
         end
       default: 
-          if (en)
+          if (en && start)
             nxt_state = COUNT;
     endcase
   end
@@ -57,9 +63,10 @@ module Sqrter (
 		.areset (rst),         //   input,   width = 1, areset.reset
 		.en     (en),          //   input,   width = 1,     en.en
 		.a      (in),          //   input,  width = 32,      a.a
-		.q      (out)          //  output,  width = 32,      q.q
+		.q      (Sqrt_out)          //  output,  width = 32,      q.q
 	);
 
   assign done = done_reg;
+  assign out = result_reg;
   
 endmodule 
