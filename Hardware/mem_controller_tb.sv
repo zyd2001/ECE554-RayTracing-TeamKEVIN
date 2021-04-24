50c0418abcb021b5ed8b5a50d2f7fc58c14a0d80
johnw08 <ywang989@wisc.edu>
2021-04-23 22:38:29 -0500

    RT-47 Memory Controller done

--- Hardware/mem_controller_tb.sv
+++ Hardware/mem_controller_tb.sv
@@ -1,7 +1,7 @@
 `include "mmio_if.svh"
 `include "dma_if.svh"
 
-module memory_controller_tb();
+module mem_controller_tb();
 
     dma_if 
      #(
@@ -50,6 +50,7 @@ module memory_controller_tb();
     logic [31:0] addr_RT_MAIN[3:0];
     logic [31:0] addr_MAIN[3:0];
     logic [127:0] data_RT_MAIN[3:0];
+    logic [127:0] data_MAIN[1:0][31:0];
     assign addr_MAIN[0] = we_RT_MC[0] ? addr_RT_MAIN[0] : addr_MC_MAIN[0];
     assign addr_MAIN[1] = we_RT_MC[1] ? addr_RT_MAIN[1] : addr_MC_MAIN[1];
     assign addr_MAIN[2] = we_RT_MC[2] ? addr_RT_MAIN[2] : addr_MC_MAIN[2];
@@ -61,12 +62,12 @@ module memory_controller_tb();
 	assign re_MC_MAIN_REAL[3] = re_MC_MAIN;
     mem_main main_IDUT(.clk(clk),   
                         .rst_n(rst_n),
-                        .we_RT(we_RT_MC),
-                        .re_RT(re_MC_MAIN_REAL),
-                        .addr_RT(addr_MAIN),
-                        .data_RT_in(data_RT_MAIN),
-                        .data_RT_out(out_data_MAIN_MC),
-                        .rd_rdy_RT());
+                        .we(we_RT_MC),
+                        .re(re_MC_MAIN_REAL),
+                        .addr(addr_MAIN),
+                        .data_in(data_RT_MAIN),
+                        .data_out(out_data_MAIN_MC),
+                        .rd_rdy());
 
 
     always #5 clk = ~clk;
@@ -85,7 +86,7 @@ module memory_controller_tb();
 
         mem_wr_strt_CP_MC = 0;
         mem_wr_rdy_tri_TRI_MC = 0;
-        patch_done_PD_MC = 1;
+        patch_done_PD_MC = 0;
         addr_RT_MAIN[0] = 32'h0000FFF0;
         addr_RT_MAIN[1] = 32'h0001FFF0;
         addr_RT_MAIN[2] = 32'h0002FFF0;
@@ -124,6 +125,8 @@ module memory_controller_tb();
         dma.empty = 1;
         dma.rd_done = 0;
         dma.rd_data = 512'h0;
+	dma.wr_done = 1'h0;
+	dma.full = 1;
 
         @(negedge clk);
         rst_n = 0;
@@ -155,8 +158,8 @@ module memory_controller_tb();
 	mmio.wr_en = 0;
         repeat(2) @(negedge clk);
         mmio.wr_en = 1;
-        mmio.wr_addr = mmio_wr_addr[1];
-        mmio.wr_data = mmio_wr_data[1];
+        mmio.wr_addr = mmio_wr_addr[4];
+        mmio.wr_data = mmio_wr_data[4];
 	@(negedge clk);
 	mmio.wr_en = 0;
 
@@ -364,9 +367,198 @@ module memory_controller_tb();
               $stop();
         end
 
+        for (int i = 0; i < 2; i++) begin
+          for (int j = 0; j < 32; j++) begin
+            data_MAIN[i][j] = {$randome, $random, $random, $random};
+          end
+        end
+
+        for (int k = 0; k < 2; k++) begin
+
+            we_RT_MC[0] = 1'h1;
+            we_RT_MC[1] = 1'h1;
+            we_RT_MC[2] = 1'h1;
+            we_RT_MC[3] = 1'h1;
+            
+            addr_RT_MAIN[0] = 32'h0000FFF0;
+            addr_RT_MAIN[1] = 32'h0001FFF0;
+            addr_RT_MAIN[2] = 32'h0002FFF0;
+            addr_RT_MAIN[3] = 32'h0003FFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][0];
+            data_RT_MAIN[1] = data_MAIN[k][1];
+            data_RT_MAIN[2] = data_MAIN[k][2];
+            data_RT_MAIN[3] = data_MAIN[k][3];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h0004FFF0;
+            addr_RT_MAIN[1] = 32'h0005FFF0;
+            addr_RT_MAIN[2] = 32'h0006FFF0;
+            addr_RT_MAIN[3] = 32'h0007FFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][4];
+            data_RT_MAIN[1] = data_MAIN[k][5];
+            data_RT_MAIN[2] = data_MAIN[k][6];
+            data_RT_MAIN[3] = data_MAIN[k][7];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h0008FFF0;
+            addr_RT_MAIN[1] = 32'h0009FFF0;
+            addr_RT_MAIN[2] = 32'h000AFFF0;
+            addr_RT_MAIN[3] = 32'h000BFFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][8];
+            data_RT_MAIN[1] = data_MAIN[k][9];
+            data_RT_MAIN[2] = data_MAIN[k][10];
+            data_RT_MAIN[3] = data_MAIN[k][11];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h000CFFF0;
+            addr_RT_MAIN[1] = 32'h000DFFF0;
+            addr_RT_MAIN[2] = 32'h000EFFF0;
+            addr_RT_MAIN[3] = 32'h000FFFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][12];
+            data_RT_MAIN[1] = data_MAIN[k][13];
+            data_RT_MAIN[2] = data_MAIN[k][14];
+            data_RT_MAIN[3] = data_MAIN[k][15];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h0010FFF0;
+            addr_RT_MAIN[1] = 32'h0011FFF0;
+            addr_RT_MAIN[2] = 32'h0012FFF0;
+            addr_RT_MAIN[3] = 32'h0013FFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][16];
+            data_RT_MAIN[1] = data_MAIN[k][17];
+            data_RT_MAIN[2] = data_MAIN[k][18];
+            data_RT_MAIN[3] = data_MAIN[k][19];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h0014FFF0;
+            addr_RT_MAIN[1] = 32'h0015FFF0;
+            addr_RT_MAIN[2] = 32'h0016FFF0;
+            addr_RT_MAIN[3] = 32'h0017FFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][20];
+            data_RT_MAIN[1] = data_MAIN[k][21];
+            data_RT_MAIN[2] = data_MAIN[k][22];
+            data_RT_MAIN[3] = data_MAIN[k][23];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h0018FFF0;
+            addr_RT_MAIN[1] = 32'h0019FFF0;
+            addr_RT_MAIN[2] = 32'h001AFFF0;
+            addr_RT_MAIN[3] = 32'h001BFFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][24];
+            data_RT_MAIN[1] = data_MAIN[k][25];
+            data_RT_MAIN[2] = data_MAIN[k][26];
+            data_RT_MAIN[3] = data_MAIN[k][27];
+
+            @(negedge clk);
+
+            addr_RT_MAIN[0] = 32'h001CFFF0;
+            addr_RT_MAIN[1] = 32'h001DFFF0;
+            addr_RT_MAIN[2] = 32'h001EFFF0;
+            addr_RT_MAIN[3] = 32'h001FFFF0;
+
+            data_RT_MAIN[0] = data_MAIN[k][28];
+            data_RT_MAIN[1] = data_MAIN[k][29];
+            data_RT_MAIN[2] = data_MAIN[k][30];
+            data_RT_MAIN[3] = data_MAIN[k][31];
+
+            @(negedge clk);
+
+            we_RT_MC[0] = 1'h0;
+            we_RT_MC[1] = 1'h0;
+            we_RT_MC[2] = 1'h0;
+            we_RT_MC[3] = 1'h0;
+
+            repeat(30) @(negedge clk);
+
+            
+
+            @(posedge clk);
+            patch_done_PD_MC = 1'h1;
+            @(negedge clk);
+            if (dma.wr_go !== 1'h1) begin
+              $display("Error: dma.wr_go incorrect");
+              $stop();
+            end
+            if (dma.wr_addr !== (mmio_wr_data[4] + k*8)) begin
+              $display("Error: dma.wr_addr. Expected: %d; Actual: %d.", mmio_wr_data[4], dma.wr_data);
+              $stop();
+            end
+            if (dma.wr_size !== 8) begin
+              $display("Error: dma.wr_size. Expected: 8; Actual: %d.", dma.wr_size);
+              $stop();
+            end
+
+            dma.full = 1'h1;
+		        @(posedge clk);
+            patch_done_PD_MC = 1'h0;
+
+            for (int i = 0; i < 8; i++) begin
+              repeat(30) @(posedge clk);
+              if (dma.wr_en === 1'h1) begin
+                $display("Error: dma.wr_en incorrect.");
+                $stop();
+              end
+              repeat(30) @(posedge clk);
+              dma.full = 1'h0;
+              @(negedge clk);
+              if (dma.wr_en !== 1'h1) begin
+                $display("Error: dma.wr_en incorrect.");
+                $stop();
+              end
+              if (dma.wr_data !== {data_MAIN[k][i*4+3], data_MAIN[k][i*4+2], data_MAIN[k][i*4+1], data_MAIN[k][i*4]}) begin
+                $display("Error: dma.wr_data incorrect.");
+                $display("Expected: %d", {data_MAIN[k][i*4+3], data_MAIN[k][i*4+2], data_MAIN[k][i*4+1], data_MAIN[k][i*4]});
+                $display("Actual: %d", dma.wr_data);
+                $stop();
+              end
+              @(posedge clk);
+              dma.full = 1'h1;
+            end
+
+            repeat(5) @(posedge clk);
+            dma.wr_done = 1'h1;
+		        @(posedge clk);
+		        dma.wr_done = 1'h0;
+            @(negedge clk);
+            if (k === 1) begin
+              if (mmio.rd_data !== 64'h2) begin
+                $display("Error: dma.wr_done incorrect. %d", mmio.rd_data);
+                $stop();
+              end
+            end
+            else begin
+              if (mmio.rd_data !== 64'h1) begin
+              $display("Error: dma.wr_done incorrect.");
+              $stop();
+              end
+            end
+            repeat(10)@(negedge clk);
+            mmio.wr_en = 1'h1;
+            mmio.wr_addr = 32'h1;
+		 
+		        @(negedge clk);
+		        mmio.wr_en = 1'h0;
+            repeat(50)@(negedge clk);
+        
+        end
 
         $display("Test Passed");
         $stop();
+
+
 
     end
 
