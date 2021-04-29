524f2e34a99534ff2c360dbec4143496a17851c0
DiKev <cevinding1997@gmail.com>
2021-04-29 14:51:42 +0800

    v3

--- Hardware/IC/v3/Calculation_v3.sv
+++ Hardware/IC/v3/Calculation_v3.sv
@@ -1,4 +1,4 @@
-module Calculation(
+module Calculation_v3(
   clk, rst, 
   v0, v1, v2, orig, dir, sid_in,
   sid_out, Intersection_Point, norm, t);
@@ -11,11 +11,10 @@ module Calculation(
   output [95:0] Intersection_Point, norm;
   
   logic hit;
-  logic [31:0] det, invDet, upre, vpre, tpre, u, v, sid_ff_out, Det_ff_out;
+  logic [31:0] det, invDet, upre, vpre, tpre, u, v, sid_ff_out;
   logic [95:0] v0v2, v0v1, tvec,
-               pvec, qvec, Intersection_Point_in, Pre_ff_out;
-  logic [287:0] PrePro_ff_out;
-  
+               pvec, qvec, Intersection_Point_in;
+
   fifo ff (
     .clk(clk), .rst(rst), .en(1'b1),
     .d(sid_in), .q(sid_out)
@@ -27,11 +26,6 @@ module Calculation(
     .v0v2(v0v2), .v0v1(v0v1), .tvec(tvec)
   );
     
-  fifo #(6, 288) PrePro_ff (
-    .clk(clk), .rst(rst), .en(1'b1),
-    .d({v0v1, v0v2, tvec}), .q(PrePro_ff_out)
-  );
-  
   CrossProduct_v2 CrossP1 (
     .clk(clk), .rst(rst),
     .vector1(dir), .vector2(v0v2),
@@ -52,13 +46,13 @@ module Calculation(
   
   DotProduct_v2 DotP1 (
     .clk(clk), .rst(rst),
-    .vector1(PrePro_ff_out[287:192]), .vector2(pvec),
+    .vector1(v0v1), .vector2(pvec),
     .result(det)
   );
   
   DotProduct_v2 DotP2 (
     .clk(clk), .rst(rst),
-    .vector1(PrePro_ff_out[95:0]), .vector2(pvec),
+    .vector1(tvec), .vector2(pvec),
     .result(upre)
   );
   
@@ -70,18 +64,8 @@ module Calculation(
   
   DotProduct_v2 DotP4 (
     .clk(clk), .rst(rst),
-    .vector1(PrePro_ff_out[191:96]), .vector2(qvec),
+    .vector1(v0v2), .vector2(qvec),
     .result(tpre)
-  );
-  
-  fifo #(17, 96) Pre_ff (
-    .clk(clk), .rst(rst), .en(1'b1),
-    .d({upre, vpre, tpre}), .q(Pre_ff_out)
-  );
-  
-  fifo #(20, 32) Det_ff (
-    .clk(clk), .rst(rst), .en(1'b1),
-    .d(det), .q(Det_ff_out)
   );
   
   Float_Inv inverse (
@@ -95,7 +79,7 @@ module Calculation(
 		.clk    (clk),      //   input,   width = 1,    clk.clk
 		.areset (rst),      //   input,   width = 1, areset.reset
 		.en     (1'b1),     //   input,   width = 1,     en.en
-		.a      (Pre_ff_out[95:64]),     //   input,  width = 32,      a.a
+		.a      (upre),     //   input,  width = 32,      a.a
 		.b      (invDet),   //   input,  width = 32,      b.b
 		.q      (u)         //  output,  width = 32,      q.q
 	);
@@ -104,7 +88,7 @@ module Calculation(
 		.clk    (clk),      //   input,   width = 1,    clk.clk
 		.areset (rst),      //   input,   width = 1, areset.reset
 		.en     (1'b1),     //   input,   width = 1,     en.en
-		.a      (Pre_ff_out[63:32]),     //   input,  width = 32,      a.a
+		.a      (vpre),     //   input,  width = 32,      a.a
 		.b      (invDet),   //   input,  width = 32,      b.b
 		.q      (v)         //  output,  width = 32,      q.q
 	);
@@ -113,14 +97,14 @@ module Calculation(
 		.clk    (clk),      //   input,   width = 1,    clk.clk
 		.areset (rst),      //   input,   width = 1, areset.reset
 		.en     (1'b1),     //   input,   width = 1,     en.en
-		.a      (Pre_ff_out[31:0]),     //   input,  width = 32,      a.a
+		.a      (tpre),     //   input,  width = 32,      a.a
 		.b      (invDet),   //   input,  width = 32,      b.b
 		.q      (t)         //  output,  width = 32,      q.q
 	);
   
   ContactDetect_v2 ConDet (
     .clk(clk), .rst(rst),
-    .u(u), .v(v), .det(Det_ff_out), 
+    .u(u), .v(v), .det(det), 
     .result(hit)
   );
     
--- Hardware/IC/v3/TriManager_v3.sv
+++ Hardware/IC/v3/TriManager_v3.sv
@@ -1,32 +1,31 @@
-module TriManager_v2(
+module TriManager_v3(
   clk, rst,
-  Mem_Rdy, Fetch, clear,
+  Mem_Rdy, Fetch,
   v1_in, v1_out,
   v2_in, v2_out,
   v0_in, v0_out,
   sid_in, sid_out,
   Mem_NotValid, Mem_En,
-  triangle_id,
-  Tri_Rdy);
+  triangle_id);
 
   parameter NUM_TRIANGLE = 512;
   localparam BIT_TRIANGLE = $clog2(NUM_TRIANGLE);
   
   input clk, rst;
-  input Mem_Rdy, Fetch, clear;
+  input Mem_Rdy, Fetch;
   input [95:0] v1_in, v2_in, v0_in;
   input [31:0] sid_in;
   input Mem_NotValid;
 
-  output Mem_En, Tri_Rdy;
+  output Mem_En;
   output [95:0] v1_out, v2_out, v0_out;
   output [31:0] sid_out;
   output unsigned [BIT_TRIANGLE-1:0] triangle_id;
 
-  typedef enum reg [1:0] {FTCH, END, IDLE} state_t;
+  typedef enum reg {FTCH, IDLE} state_t;
   state_t state, nxt_state;
   
-  logic ld, Tri_Rdy_in, Tri_Rdy_reg, Mem_En_in;
+  logic ld, Mem_En_in, clear;
   logic [95:0] v1_reg, v2_reg, v0_reg;
   logic [31:0] sid_reg;
   logic unsigned [BIT_TRIANGLE-1:0] triangle_id_reg;
@@ -44,7 +43,6 @@ module TriManager_v2(
       v2_reg <= '0;
       v0_reg <= '0;
       sid_reg <= '0;
-      Tri_Rdy_reg <= '0;
       triangle_id_reg <= '0;
     end
     else if (ld) begin
@@ -52,47 +50,29 @@ module TriManager_v2(
       v2_reg <= v2_in;
       v0_reg <= v0_in;
       sid_reg <= sid_in;
-      Tri_Rdy_reg <= Tri_Rdy_in;
       triangle_id_reg <= triangle_id_reg + 1'b1;
     end
-    else 
-      Tri_Rdy_reg <= Tri_Rdy_in;
   end
 
   always_comb begin
     Mem_En_in = 1'b0;
+    clear = 1'b0;
     ld = 1'b0;
-    Tri_Rdy_in = 1'b0;
     nxt_state = IDLE;
     case(state)
-      END:
-        begin
-          if (!clear)
-            nxt_state = END;
-        end
       FTCH: 
         begin
-          if (Mem_Rdy) begin
-            if (sid_in == '0)
-              nxt_state = END;
-            else begin
-              Tri_Rdy_in = 1'b1;
-              ld = 1'b1;
-            end
-          end
-          else begin
-            if (!Mem_NotValid) begin
-              Mem_En_in = 1'b1;
-            end
+          if (Mem_NotValid)
+            clear = 1'b1;
+          else if (Mem_Rdy)
+            ld = 1'b1;
+          else 
             nxt_state = FTCH;
-          end
         end
       default: 
         begin
           if (Fetch) begin
-            if (!Mem_NotValid) begin
-              Mem_En_in = 1'b1;
-            end
+            Mem_En_in = 1'b1;
             nxt_state = FTCH;
           end
         end
@@ -100,7 +80,6 @@ module TriManager_v2(
   end  
 
   assign Mem_En = Mem_En_in;
-  assign Tri_Rdy = Tri_Rdy_reg;
   assign triangle_id = triangle_id_reg;
   assign v1_out = v1_reg;
   assign v2_out = v2_reg;
