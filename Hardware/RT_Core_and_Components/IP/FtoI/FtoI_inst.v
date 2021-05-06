0493c977517e136f628661d59a64a6ab1afc65e0
Jialuo Gao <jialuogao980513@gmail.com>
2021-05-05 23:47:24 -0500

    remove generated files

--- Hardware/RT_Core_and_Components/IP/FtoI/FtoI_inst.v
+++
@@ -1,8 +0,0 @@
-	FtoI u0 (
-		.clk    (_connected_to_clk_),    //   input,   width = 1,    clk.clk
-		.areset (_connected_to_areset_), //   input,   width = 1, areset.reset
-		.en     (_connected_to_en_),     //   input,   width = 1,     en.en
-		.a      (_connected_to_a_),      //   input,  width = 32,      a.a
-		.q      (_connected_to_q_)       //  output,  width = 32,      q.q
-	);
-
