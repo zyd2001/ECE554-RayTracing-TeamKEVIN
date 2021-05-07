c39304467c403560a0e4e9b75780f60865b9721f
Jialuo Gao <jialuogao980513@gmail.com>
2021-05-07 06:41:01 -0500

    update gitignore

--- Hardware/RT_Core_and_Components/IP/100hz/Float_Grtr/Float_Grtr_inst.v
+++
@@ -1,9 +0,0 @@
-	Float_Grtr u0 (
-		.clk    (_connected_to_clk_),    //   input,   width = 1,    clk.clk
-		.areset (_connected_to_areset_), //   input,   width = 1, areset.reset
-		.en     (_connected_to_en_),     //   input,   width = 1,     en.en
-		.a      (_connected_to_a_),      //   input,  width = 32,      a.a
-		.b      (_connected_to_b_),      //   input,  width = 32,      b.b
-		.q      (_connected_to_q_)       //  output,   width = 1,      q.q
-	);
-
