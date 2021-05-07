e8c1c7fcdc2713147646c827d86be23cf3a04f88
Jialuo Gao <jialuogao980513@gmail.com>
2021-05-07 02:15:39 -0500

    minor

--- Hardware/tests/mem_main_full_tb.sv
+++ Hardware/tests/mem_main_full_tb.sv
@@ -23,8 +23,7 @@ module mem_main_full_tb();
 
     always #1 clk = ~clk;
 
-
-    mem_main #(NUM_RT, NUM_THREAD, NUM_BANK_PTHREAD) main(clk, rst_n, we_RT, re_RT, addr_RT, data_RT_in,
+    mem_main #(NUM_RT, NUM_THREAD, NUM_BANK_PTHREAD) main(clk, rst_n, we_RT, re_RT, '{1'b1,1'b1,1'b1,1'b1}, addr_RT, data_RT_in,
                   data_RT_out, rd_rdy_RT);
     
     logic [127:0] data_RT_copy[NUM_RT+1:0];
