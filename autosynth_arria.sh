93fdf3551f2b5fd8f57453ff135f39c766044ff1
Jialuo Gao <jialuogao980513@gmail.com>
2021-05-09 13:15:06 -0500

    script for arria

---
+++ autosynth_arria.sh
@@ -0,0 +1,17 @@
+frequency='200'
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Fix_Add
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Fix_Div
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Fix_Mul
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Add
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Div
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Grtr
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Inv
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Less
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Mul
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/Float_Sqrt
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/FtoI
+rm -rf Hardware/RT_Core_and_Components/IP/Arria_$frequency/ItoF
+rm -rf synth_Arria_$frequency
+afu_synth_setup --source Hardware/filelist_synth_Arria_$frequency.txt synth_Arria_$frequency
+cd synth_Arria_$frequency
+${OPAE_PLATFORM_ROOT}/bin/run.sh
