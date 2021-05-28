be53ce46a67ba84e57dfdcdf7168a8ff81b7819c
zyd2001 <zyd200101@outlook.com>
2021-05-28 13:58:30 +0800

    new triangle file

--- Software/Assembler/triangle.py
+++ Software/Assembler/triangle.py
@@ -2,15 +2,16 @@
 
 import sys
 import struct
+
+name = sys.argv[1].split('.')[0]
+
 objfile = open(sys.argv[1])
-mtlfile = open(sys.argv[1]+".mtl")
+mtlfile = open(name + ".mtl")
 
 material_list = {}
 
-material = {}
-
 current_mtl = None
-counter = 2
+counter = 1
 for line in mtlfile:
     line = line.split()
     if line:
@@ -33,12 +34,8 @@ cmtl = 0
 for line in objfile:
     line = line.split()
     if line:
-        # if line[0] == "o":
-        #     current_object = {"vertex": [], "triangle": []}
-        #     object_list.append(current_object)
-        # if current_object:
+        
         if (line[0] == "v"):
-            # current_object["vertex"].append((float(line[1]), float(line[2]), float(line[3])))
             vertex.append((float(line[1]), float(line[2]), float(line[3])))
         if (line[0] == "usemtl"):
             cmtl = material_list[line[1]]["id"]
@@ -50,51 +47,26 @@ for line in objfile:
             if cmtl == 0:
                 raise Exception('fv<k')
             t.append(cmtl)
-            # current_object["triangle"].append(t)
             triangle.append(t)
 
-# vertex = []
-# triangle = []
 
-# for line in objfile:
-#     line = line.split()
-#     if (len(line)):
-#         if (line[0] == "v"):
-#             vertex.append((float(line[1]), float(line[2]), float(line[3])))
-#         if (line[0] == "f"):
-#             t = [0,0,0]
-#             for i in range(1, 4):
-#                 p = line[i].split('/')
-#                 t[i-1] = int(p[0])
-#             triangle.append(t)
 f = 1
 if (len(sys.argv) == 2):
-    f = open(sys.argv[1] + '.out', "wb")
+    f = open(name + '.out', "wb")
 else:
-    f = open(sys.argv[1] + '.out', "w")
+    f = open(name + '.out', "w")
 
 
 if (len(sys.argv) == 2):
     for ind, t in enumerate(triangle):
-        # b = bytearray()
-        # b.extend(struct.pack("i", ind + 1))
         for i in range(4):
-        # for i in range(2,-1, -1):
-            # b.extend(struct.pack("i", t[i]-1))
             f.write(struct.pack("i", t[i]-1))
-        # b.reverse()
-        # f.write(b)
     for i in range(4):
         f.write(struct.pack("i", 0))
     for v in vertex:
-        # b = bytearray()
-        # b.extend(struct.pack("i", 123))
         for i in range(3):
-        # for i in range(2,-1, -1):
-            # b.extend(struct.pack("f", v[i]))
             f.write(struct.pack("f", v[i]))
         f.write(struct.pack("i", 123))
-        # f.write(b)
     for i in range(4):
         f.write(struct.pack("i", 0))
 else:
@@ -105,5 +77,23 @@ else:
                 f.write(str(v[j]) + ' ')
             f.write('\n')
         f.write(str(t[3] - 1) + '\n')
+
+file = open(name + '.material', 'w')
+
+for index in material_list:
+    m = material_list[index]
+    file.write('Material ' + index + '\n')
+    file.write(', '.join([str(num) for num in m['color']]))
+    file.write(', 1.0\n')
+    if 'roughness' in m:
+        file.write(m['roughness'])
+    else:
+        file.write('1.0')
+    file.write(', ')
+    if 'metal' in m:
+        file.write(m['metal'])
+    else:
+        file.write('0.0')
+    file.write(', 0, 0\n')
 
 f.close()
